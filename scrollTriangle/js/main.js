33e4839384d1f5ddb8873048478a08c9d0f9918a
iMac20 Infra <imac20infra@iMac-de-iMac20-Infra.local>
2016-05-31 11:24:14 +0200

    scrollTriangle  mouvement des triangle en intéraction avec le scroll

--- scrollTriangle/js/main.js
+++ scrollTriangle/js/main.js
@@ -1,37 +1,23 @@
 
-/*var s = Snap("#triangle");
-
-var tri1 = new Triangle(10, 10, 10, 300, 100, 200, 150);
-tri1.move(2, 500, mina.bounce);
-*/
-
 var s = Snap("#triangle");
-var tri1 = new Triangle(10, 10, 10, 300, 100, 200, 150);
+
+var y1 = [10,200,300,850, 1300];
+var y2 = [300, 500, 900, 1500, 1400];
+
+var tri1 = new Triangle(0, y1[0], 0, y2[0], 100, 200, 150);
+var tri2 = new Triangle(0, y1[1], 0, y2[1], 150, 350, 150);
+var tri3 = new Triangle(0, y1[2], 0, y2[2], 100, 800, 400);
+var tri4 = new Triangle(0, y1[3], 0, y2[3], 150, 950, 400);
+var tri5 = new Triangle(0, y1[4], 0, y2[4], 40, 1320, 50);
+
+var triangles= [ tri1, tri2, tri3, tri3, tri4, tri5]; 
 
 
-
-/*
-var s = Snap("#triangle");
-//le dégradé
-var rgba= "rgba(245,5,49,0.8)-rgba(138,1,1,1)";
-//la position du dégradé + le dégardé
-var grad1 = "R(40, 200, 150)"+rgba;
-var z = triangle.gradient(grad1);
-var tri1 = triangle.group(triangle.create(10,10,10,300,100,200).attr({ fill: z }));
-
-var grad2 = "R(70, 350, 150)"+rgba;
-var z = triangle.gradient(grad2);
-var tri2 = triangle.group(triangle.create(10,200,10,500,150,350).attr({ fill: z }));
-
-var grad3 = "R(50, 800, 400)"+rgba;
-var z = triangle.gradient(grad3);
-var tri3 = triangle.group(triangle.create(10,300,10,900,100,800).attr({ fill: z }));
-
-var grad4 = "R(100, 950, 400)"+rgba;
-var z = triangle.gradient(grad4);
-var tri4 = triangle.group(triangle.create(10,850,10,1500,150,950).attr({ fill: z }));
-
-var grad5 = "R(20, 1320, 30)"+rgba;
-var z = triangle.gradient(grad5);
-var tri5 = triangle.group(triangle.create(10,1300,10,1400,40,1320).attr({ fill: z }));
-*/
+$(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    for(var i = 0; i < y2.length; i++){
+        if (scroll >= y1[i] || scroll <= y2[i]){
+            triangles[i].move(2, 300, mina.bounce);
+        }
+    }
+});
